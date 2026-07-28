14762ca4453ffec8f8d2693fea56145c7b90cb1b
mskim-17 <mskim17.public@gmail.com>
2026-07-28 11:07:36 +0900

    chore: initial commit, add *.properties to .gitignore

---
+++ src/main/java/com/example/chook/ChookApplication.java
@@ -0,0 +1,13 @@
+package com.example.chook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChookApplication.class, args);
+	}
+
+}
