280ea9e82d1dfd48b9345c72495a99e61de26cfb
mskim-17 <mskim17.public@gmail.com>
2026-07-30 01:27:55 +0900

    feat: region Service 구현체 및 필요한 쿼리 메서드 추가

---
+++ src/main/java/com/example/chook/entity/region/RegionServiceImpl.java
@@ -0,0 +1,36 @@
+package com.example.chook.entity.region;
+
+import com.example.chook.entity.RegionSido;
+import com.example.chook.entity.RegionSigungu;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Slf4j
+public class RegionServiceImpl implements RegionService {
+
+  private final RegionSidoRepository regionSidoRepository;
+  private final RegionSigunguRepository regionSigunguRepository;
+
+  @Override
+  public List<RegionDTO> getSidoList() {
+    List<RegionSido> sidoList = regionSidoRepository.findAll(
+      Sort.by(Sort.Direction.ASC, "sidoCode")
+    );
+    return sidoList.stream().map(this::toDto).toList();
+  }
+
+  @Override
+  public List<RegionDTO> getSigunguList(String sidoCode) {
+    List<RegionSigungu> sigunguList = regionSigunguRepository.findBySido_SidoCode(
+      sidoCode,
+      Sort.by(Sort.Direction.ASC, "sigunguName")
+    );
+    return sigunguList.stream().map(this::toDto).toList();
+  }
+}
--- src/main/java/com/example/chook/entity/region/RegionSigunguRepository.java
+++ src/main/java/com/example/chook/entity/region/RegionSigunguRepository.java
@@ -1,7 +1,13 @@
 package com.example.chook.entity.region;
 
 import com.example.chook.entity.RegionSigungu;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface RegionSigunguRepository extends JpaRepository<RegionSigungu, String> {
+
+  List<RegionSigungu> findBySido_SidoCode(String sidoCode, Sort sort);
+
 }
