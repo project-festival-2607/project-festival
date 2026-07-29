f87cd9686b997ba808e726d8d20bc1373cc3eb87
mskim-17 <mskim17.public@gmail.com>
2026-07-30 00:56:54 +0900

    feat: region의 각 entity에 해당하는 repository 생성

---
+++ src/main/java/com/example/chook/entity/region/RegionSidoRepository.java
@@ -0,0 +1,7 @@
+package com.example.chook.entity.region;
+
+import com.example.chook.entity.RegionSido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionSidoRepository extends JpaRepository<RegionSido, String> {
+}
---
+++ src/main/java/com/example/chook/entity/region/RegionSigunguRepository.java
@@ -0,0 +1,7 @@
+package com.example.chook.entity.region;
+
+import com.example.chook.entity.RegionSigungu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RegionSigunguRepository extends JpaRepository<RegionSigungu, String> {
+}
