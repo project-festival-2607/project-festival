b0f17391df465a774857c1b1b1d4deea05ecdac1
mskim-17 <mskim17.public@gmail.com>
2026-07-30 12:15:25 +0900

    feat: UploadedFileCategory에 MimeType으로부터의 변환을 위한 메서드 추가

--- src/main/java/com/example/chook/entity/enums/UploadedFileCategory.java
+++ src/main/java/com/example/chook/entity/enums/UploadedFileCategory.java
@@ -3,6 +3,8 @@ package com.example.chook.entity.enums;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import java.util.stream.Stream;
+
 @Getter
 @RequiredArgsConstructor
 public enum UploadedFileCategory {
@@ -15,5 +17,14 @@ public enum UploadedFileCategory {
   OTHERS("others");
 
   private final String value;
+
+  public static UploadedFileCategory fromMimeType(String mimeType) {
+    if (mimeType == null) return OTHERS;
+    String mimePrefix = mimeType.split("/")[0];
+    return Stream.of(values())
+      .filter(category -> category.value.equalsIgnoreCase(mimePrefix))
+      .findFirst()
+      .orElse(OTHERS);
+  }
 
 }
