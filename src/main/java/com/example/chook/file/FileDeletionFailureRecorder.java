fa5a2f5324edac65120fd51cc95eed0d3ac06427
mskim-17 <mskim17.public@gmail.com>
2026-07-31 15:51:55 +0900

    feat: CSV 작업을 Recorder만 수행하도록 RetryService 구조 변경

--- src/main/java/com/example/chook/file/FileDeletionFailureRecorder.java
+++ src/main/java/com/example/chook/file/FileDeletionFailureRecorder.java
@@ -3,15 +3,20 @@ package com.example.chook.file;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Component
@@ -20,7 +25,43 @@ public class FileDeletionFailureRecorder {
 
   private final FileSystemProperties properties;
 
-  public synchronized void clear() {
+  private static final CSVFormat CSV_FORMAT = CSVFormat.DEFAULT.builder()
+    .setHeader()
+    .setSkipHeaderRecord(true).get();
+
+  public synchronized List<FileDeletionFailureRecord> load() {
+
+    Path absoluteSystemDir = Paths.get(properties.getSystemDir());
+    Path deleteFailedLogPath = absoluteSystemDir.resolve(properties.getDeleteFailLogFile());
+
+    List<FileDeletionFailureRecord> fileDeletionFailureRecords = new ArrayList<>();
+
+    if (Files.notExists(deleteFailedLogPath)) return new ArrayList<>();  // 기록 자체가 없는 경우
+
+    try (CSVParser parser = CSVParser.parse(deleteFailedLogPath, StandardCharsets.UTF_8, CSV_FORMAT)) {
+      for (CSVRecord record : parser) {
+        fileDeletionFailureRecords.add(new FileDeletionFailureRecord(
+          record.get("uuid_str"),
+          record.get("relative_path"),
+          record.get("stored_name")
+        ));
+      }
+    } catch (IOException exception) {
+      log.error("파일 삭제 실패 로그 읽기 실패", exception);
+    }
+
+    return fileDeletionFailureRecords;
+
+  }
+
+  public synchronized void update(List<FileDeletionFailureRecord> failureRecords) {
+    clear();
+    for (FileDeletionFailureRecord record : failureRecords) {
+      recordDeleteFailure(record);
+    }
+  }
+
+  private synchronized void clear() {
 
     Path absoluteSystemDir = Paths.get(properties.getSystemDir());
     Path deleteFailedLogPath = absoluteSystemDir.resolve(properties.getDeleteFailLogFile());
--- src/main/java/com/example/chook/file/service/FileDeletionRetryService.java
+++ src/main/java/com/example/chook/file/service/FileDeletionRetryService.java
@@ -3,21 +3,12 @@ package com.example.chook.file.service;
 import com.example.chook.file.FileDeletionFailureRecord;
 import com.example.chook.file.FileDeletionFailureRecorder;
 import com.example.chook.file.FileStorage;
-import com.example.chook.file.FileSystemProperties;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,49 +18,21 @@ import java.util.List;
 @RequiredArgsConstructor
 public class FileDeletionRetryService {
 
-  private final FileSystemProperties properties;
   private final FileDeletionFailureRecorder failureRecorder;
   private final FileStorage fileStorage;
-
-  private static final CSVFormat CSV_FORMAT = CSVFormat.DEFAULT.builder()
-    .setHeader()
-    .setSkipHeaderRecord(true).get();
 
   @Scheduled(cron = "00 */10 * * * *")
   public synchronized void retryFileDeletion() {
 
     log.info("파일 재삭제 작업 시작");
-    Path absoluteSystemDir = Paths.get(properties.getSystemDir());
-    Path deleteFailedLogPath = absoluteSystemDir.resolve(properties.getDeleteFailLogFile());
 
-    List<FileDeletionFailureRecord> deleteFailedRecords = loadDeleteFailedRecord(deleteFailedLogPath);
+    List<FileDeletionFailureRecord> deleteFailedRecords = failureRecorder.load();
     List<FileDeletionFailureRecord> newDeleteFailedRecords =
       deleteFilesFromFailedRecords(deleteFailedRecords);
-    writeDeleteFailedRecord(newDeleteFailedRecords);
+    failureRecorder.update(newDeleteFailedRecords);
 
     log.info("파일 재삭제 작업 종료");
 
-  }
-
-  private List<FileDeletionFailureRecord> loadDeleteFailedRecord(Path path) {
-
-    List<FileDeletionFailureRecord> deleteFailedRecords = new ArrayList<>();
-
-    if (Files.notExists(path)) return new ArrayList<>();  // 기록 자체가 없는 경우
-
-    try (CSVParser parser = CSVParser.parse(path, StandardCharsets.UTF_8, CSV_FORMAT)) {
-      for (CSVRecord record : parser) {
-        deleteFailedRecords.add(new FileDeletionFailureRecord(
-          record.get("uuid_str"),
-          record.get("relative_path"),
-          record.get("stored_name")
-        ));
-      }
-    } catch (IOException exception) {
-      log.error("파일 삭제 실패 로그 읽기 실패", exception);
-    }
-
-    return deleteFailedRecords;
   }
 
   private List<FileDeletionFailureRecord> deleteFilesFromFailedRecords(List<FileDeletionFailureRecord> deleteFailedRecords) {
@@ -84,15 +47,6 @@ public class FileDeletionRetryService {
     }
 
     return newDeleteFailedRecords;
-
-  }
-
-  private void writeDeleteFailedRecord(List<FileDeletionFailureRecord> newDeleteFailedRecords) {
-
-    failureRecorder.clear();
-    for (FileDeletionFailureRecord deleteFailedRecord : newDeleteFailedRecords) {
-      failureRecorder.recordDeleteFailure(deleteFailedRecord);
-    }
 
   }
 
