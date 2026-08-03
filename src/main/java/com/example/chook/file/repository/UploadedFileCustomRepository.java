581fe991ebd3f555407b9ed0e259a5a458a1d906
mskim-17 <mskim17.public@gmail.com>
2026-08-03 20:52:51 +0900

    feat: 파일 전체 DB에는 존재하나 어느 곳에서도 사용되지 않은 파일 조회를 위한 메서드 추가

--- src/main/java/com/example/chook/file/repository/UploadedFileCustomRepository.java
+++ src/main/java/com/example/chook/file/repository/UploadedFileCustomRepository.java
@@ -2,10 +2,13 @@ package com.example.chook.file.repository;
 
 import com.example.chook.file.record.FilePath;
 
+import java.util.List;
 import java.util.Set;
 
 public interface UploadedFileCustomRepository {
 
   Set<FilePath> findAllFilePaths();
+
+  List<FilePath> findUnreferencedFilePaths();
 
 }
