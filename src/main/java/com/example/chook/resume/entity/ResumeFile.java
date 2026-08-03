b25c03077839269f87641b4bd31c73cc1fb5840f
dlwlsdn243 <dlwlsdn243@gmail.com>
2026-08-03 16:33:24 +0900

    feat - ResumeFile-entity 추가

--- src/main/java/com/example/chook/resume/entity/ResumeFile.java
+++ src/main/java/com/example/chook/resume/entity/ResumeFile.java
@@ -20,28 +20,23 @@ public class ResumeFile {
     @Column(name = "resume_file_id")
     private Long resumeFileId;
 
-    // 이력서 아이디
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(
-            name = "resume_id",
-            foreignKey = @ForeignKey(name = "fk_resume_file_resume_id")
-    )
-    @ToString.Exclude
-    private Resume resume;
-
     // 포트폴리오 아이디
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(
             name = "portfolio_id",
+            nullable = false,
+            unique = true,
             foreignKey = @ForeignKey(name = "fk_resume_file_portfolio_id")
     )
     @ToString.Exclude
     private ResumePortfolio resumePortfolio;
 
     // 파일 아이디
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(
             name = "file_id",
+            nullable = false,
+            unique = true,
             foreignKey = @ForeignKey(name = "fk_resume_file_file_id")
     )
     @ToString.Exclude
