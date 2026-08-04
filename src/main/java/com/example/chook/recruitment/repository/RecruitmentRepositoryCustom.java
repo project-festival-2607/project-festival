be69df12c99ed7ae017dcfce7cb27bd200ec546b
mskim-17 <mskim17.public@gmail.com>
2026-08-04 11:20:44 +0900

    feat: 지원 공고 검색을 위한 RepositoryCustom 생성

--- src/main/java/com/example/chook/recruitment/repository/RecruitmentRepositoryCustom.java
+++ src/main/java/com/example/chook/recruitment/repository/RecruitmentRepositoryCustom.java
@@ -1,4 +1,12 @@
 package com.example.chook.recruitment.repository;
 
+import com.example.chook.recruitment.entity.Recruitment;
+import com.example.chook.recruitment.record.RecruitmentSearchCondition;
+
+import java.util.List;
+
 public interface RecruitmentRepositoryCustom {
+
+    List<Recruitment> searchRecruitments(RecruitmentSearchCondition condition);
+
 }
