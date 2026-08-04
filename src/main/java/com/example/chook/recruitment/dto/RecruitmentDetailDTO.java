564a3ba9ed2802c74c40a7a6bb8e0d7601830162
mskim-17 <mskim17.public@gmail.com>
2026-08-04 12:17:45 +0900

    feat: 지원 공고 목록 조회 목적 DTO (RecruitmentListDTO)와 상세 조회 목적 DTO (RecruitmentDetailDTO) 추가 note: 두 DTO 사이의 차이는 content 하나 뿐이나, 추후 목록 조회 DTO가 더 간단해질 때나, content가 WYSIWIG이기 때문에 content의 크기가 유의미할 때를 대비해 나눠 구현함

---
+++ src/main/java/com/example/chook/recruitment/dto/RecruitmentDetailDTO.java
@@ -0,0 +1,45 @@
+package com.example.chook.recruitment.dto;
+
+import com.example.chook.recruitment.entity.enums.RecruitmentCategory;
+import com.example.chook.recruitment.entity.enums.RecruitmentStatus;
+import com.example.chook.recruitment.entity.enums.RecruitmentWageType;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RecruitmentDetailDTO {
+
+  private String regionSidoName;
+  private String regionSigunguName;
+
+  private Long recruitmentId;
+  private String recruitmentTitle;
+  private String festivalContentId;
+  private String festivalTitle;
+
+  private String content;
+
+  private RecruitmentCategory category;
+  private RecruitmentWageType wageType;
+  private Integer wageValue;
+  private LocalDate applicationDeadline;
+  private Integer recruitmentCount;
+  private RecruitmentStatus status;
+
+  private String workingLocation;
+  private LocalDate workingStartDate;
+  private LocalDate workingEndDate;
+  private LocalTime workingStartTime;
+  private LocalTime workingEndTime;
+
+  private LocalDateTime publishedAt;
+
+}
