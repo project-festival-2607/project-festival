929d66f445bc1eea25ac8aab6d6f199f5ca518f5
mskim-17 <mskim17.public@gmail.com>
2026-08-05 11:01:33 +0900

    chore: IntelliJ 자동 Optimize imports

--- src/main/java/com/example/chook/support/entity/InquiryFile.java
+++ src/main/java/com/example/chook/support/entity/InquiryFile.java
@@ -1,14 +1,7 @@
 package com.example.chook.support.entity;
 
 import com.example.chook.file.entity.UploadedFile;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 @Getter
