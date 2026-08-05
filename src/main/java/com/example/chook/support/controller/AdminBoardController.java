70589c6ed7146047a84ddbaf11134681946c7ec8
rooguie <rooguie@naver.com>
2026-08-05 14:31:02 +0900

    feat: 문의게시판

--- src/main/java/com/example/chook/support/controller/AdminBoardController.java
+++ src/main/java/com/example/chook/support/controller/AdminBoardController.java
@@ -111,7 +111,7 @@ public class AdminBoardController {
         @RequestParam("image") MultipartFile image,
         @RequestParam("title") String title
     ) {
-        String relativePath = toFolderName(title); // 파일 저장 경로
+        String relativePath = "adminBoard/" + toFolderName(title); // 파일 저장 경로
         FileDTO fileDto = fileService.uploadAndGetDto(image, relativePath);
         log.info("adminBoard image uploaded: {}", fileDto);
         return ResponseEntity.ok(fileDto);
