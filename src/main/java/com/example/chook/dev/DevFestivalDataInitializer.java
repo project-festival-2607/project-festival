386bf4f84d14f8736b6fdde3732bd37878f81788
mskim-17 <mskim17.public@gmail.com>
2026-08-07 00:58:37 +0900

    chore: 일부 자료형 변경 및 generateSampleFestivals() 인자 역할 변경

--- src/main/java/com/example/chook/dev/DevFestivalDataInitializer.java
+++ src/main/java/com/example/chook/dev/DevFestivalDataInitializer.java
@@ -24,16 +24,17 @@ public class DevFestivalDataInitializer {
 
   private final Random random = new Random();
 
-  public void generateSampleFestivals(int festivalCountToAdd) {
+  public void generateSampleFestivals(long targetFestivalCount) {
 
-    if (festivalRepository.count() > 0) return;
+    long festivalCountToAdd = Math.max(targetFestivalCount - festivalRepository.count(), 0);
+    if (festivalCountToAdd == 0) return;
 
     List<Member> recruiters = memberRepository.findByRole(MemberRole.RECRUITER);
     long earliestStartDateEpochDay = LocalDate.now().minusDays(7).toEpochDay();
     long latestStartDateEpochDay = LocalDate.now().plusMonths(3).toEpochDay();
     int maxFestivalDuration = 7;
 
-    for (int i = 1; i <= festivalCountToAdd; i++) {
+    for (long i = 1; i <= festivalCountToAdd; i++) {
 
       LocalDate startDate = LocalDate.ofEpochDay(random.nextLong(
         earliestStartDateEpochDay,
