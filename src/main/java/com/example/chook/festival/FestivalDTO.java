172130f90ab0670a7a9d4f0f755a6611b22c099e
mskim-17 <mskim17.public@gmail.com>
2026-08-06 23:00:34 +0900

    chore: DTO에서 MultipartFile imageFile 삭제

--- src/main/java/com/example/chook/festival/FestivalDTO.java
+++ src/main/java/com/example/chook/festival/FestivalDTO.java
@@ -1,7 +1,6 @@
 package com.example.chook.festival;
 
 import lombok.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -32,8 +31,6 @@ public class FestivalDTO {
     private String endDate;
     private String firstImage;
     private String secondImage;
-
-    private MultipartFile imageFile;
 
     public String getOfficialHomepage() {
         if (this.homepage == null || this.homepage.isBlank()) {
