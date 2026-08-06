451617f383daf34a621f2c0523ad34f669d45aa2
mskim-17 <mskim17.public@gmail.com>
2026-08-06 21:42:45 +0900

    refactor: CommandRunner를 모두 ApplicationRunner로 교체

--- src/main/java/com/example/chook/dev/DevDataInitializer.java
+++ src/main/java/com/example/chook/dev/DevDataInitializer.java
@@ -1,8 +1,9 @@
 package com.example.chook.initializer;
 
-import com.example.chook.member.service.MemberInitService;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,7 +13,7 @@ public class TestDataInitializer implements CommandLineRunner {
   private final MemberInitService memberInitService;
 
   @Override
-  public void run(String... args) throws Exception {
     memberInitService.addSampleMembers(3, 10, 5);
+  public void run(@NonNull ApplicationArguments arg) throws Exception {
   }
 }
