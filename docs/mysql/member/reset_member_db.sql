80e70e066e3736341431ef24e2273d6b665d99c4
mskim-17 <mskim17.public@gmail.com>
2026-08-06 21:12:00 +0900

    feat: 초기 실행시 관리자를 포함한 Member 추가 구현

---
+++ docs/mysql/member/reset_member_db.sql
@@ -0,0 +1,8 @@
+SET FOREIGN_KEY_CHECKS = 0;
+
+TRUNCATE TABLE business_registrations;
+TRUNCATE TABLE employer_profiles;
+TRUNCATE TABLE job_seeker_profiles;
+TRUNCATE TABLE members;
+
+SET FOREIGN_KEY_CHECKS = 1;
