bc081b56abc61d47032beb450efe4d08b6787f20
jgkim7848-lab <jgkim7848@gmail.com>
2026-08-07 17:38:48 +0900

    커피커피

--- src/main/java/com/example/chook/payment/repository/EligibleMemberView.java
+++
@@ -1,8 +0,0 @@
-package com.example.chook.payment.repository;
-
-// role 컬럼(enum 매핑 문제)을 건드리지 않기 위한 최소 정보 projection
-public interface EligibleMemberView {
-    Long getId();
-    String getUsername();
-    String getName();
-}
