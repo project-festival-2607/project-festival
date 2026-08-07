af8acd05e4df0a318e350c3ed29d3ad834b8be4a
HYUN1400 <o_ovo_024@naver.com>
2026-08-07 10:04:17 +0900

    feat: festival/map/0807

---
+++ src/main/resources/static/js/festival/festivalDetail.js
@@ -0,0 +1,9 @@
+console.log ("detail js in");
+
+const mapY = Number(`${fes.mapY}`);
+const mapX = Number(`${fes.mapX}`);
+
+const container = document.querySelector(".mapBox");
+const options = {
+
+}
