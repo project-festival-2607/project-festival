a06b4d803eedeb977b87608734850bf069730516
dlwlsdn243 <dlwlsdn243@gmail.com>
2026-08-10 14:06:02 +0900

    feat : mypage

--- src/main/java/com/example/chook/mypage/MyPageController.java
+++ src/main/java/com/example/chook/mypage/MyPageController.java
@@ -1,14 +1,13 @@
 package com.example.chook.mypage;
 
-import com.example.chook.member.dto.LoginResponseDTO;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.ui.Model;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/mypage")
@@ -20,28 +19,23 @@ public class MyPageController {
 
     // 조회기능
     @GetMapping
-    public String mypage(HttpSession session, Model model){
-
-        // 세션에서 로그인한 회원 정보 가져오기
-        LoginResponseDTO loginMember =
-                (LoginResponseDTO) session.getAttribute("loginMember");
+    public String mypage(@AuthenticationPrincipal UserDetails user, Model model){
 
         // 로그인하지 않은 경우
-        if (loginMember == null) {
+        if (user == null) {
             return "redirect:/member/login";
         }
 
-        // 로그인한 회원의 아이디
-        String username = loginMember.getUsername();
+        // 현재 로그인한 회원의 아이디
+        String username = user.getUsername();
 
         // DB에서 회원 정보 조회
         MyPageDTO myPageDTO =
                 myPageService.getMyPage(username);
 
-        // HTML로 전달
+        // HTML에 전달
         model.addAttribute("myPageDTO", myPageDTO);
 
         return "mypage/mypage";
     }
-
 }
