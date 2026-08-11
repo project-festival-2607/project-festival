dd58f1058e09f2dd776b623e4f74e11040452c69
mskim-17 <mskim17.public@gmail.com>
2026-08-11 14:43:16 +0900

    refactor: 일부 함수를 제네렉 힘수로 교체

--- src/main/java/com/example/chook/recruitment/repository/RecruitmentRepositoryCustomImpl.java
+++ src/main/java/com/example/chook/recruitment/repository/RecruitmentRepositoryCustomImpl.java
@@ -23,6 +23,7 @@ import java.time.LocalTime;
 import java.util.List;
 import java.util.Optional;
 
+import static com.example.chook.common.util.QuerydslUtils.*;
 import static com.example.chook.recruitment.entity.QRecruitment.recruitment;
 import static com.example.chook.recruitment.entity.QRecruitmentFoodTruck.recruitmentFoodTruck;
 import static com.example.chook.recruitment.entity.QRecruitmentIndividual.recruitmentIndividual;
@@ -46,14 +47,14 @@ public class RecruitmentRepositoryCustomImpl implements RecruitmentRepositoryCus
     whereCondition
       .and(recruitment.deletedAt.isNull())
       .and(containsAnyKeyword(condition.keywordList()))
-      .and(regionSidoEq(condition.regionSidoCode()))
-      .and(regionSigunguEq(condition.regionSigunguCode()))
-      .and(categoryEq(category))
-      .and(statusEq(RecruitmentStatus.RECRUITING))
-      .and(workingStartTimeGoe(condition.workingStartTime()))
-      .and(workingEndTimeLoe(condition.workingEndTime()))
-      .and(workingStartDateGoe(condition.workingStartDate()))
-      .and(workingEndDateLoe(condition.workingEndDate()))
+      .and(eq(recruitment.sigungu.sido.code, condition.regionSidoCode()))
+      .and(eq(recruitment.sigungu.code, condition.regionSigunguCode()))
+      .and(eq(recruitment.category, category))
+      .and(eq(recruitment.status, RecruitmentStatus.RECRUITING))
+      .and(loe(recruitment.workingStartDate, condition.workingStartDate()))
+      .and(goe(recruitment.workingEndDate, condition.workingEndDate()))
+      .and(goe(recruitment.workingStartTime, condition.workingStartTime()))
+      .and(loe(recruitment.workingEndTime, condition.workingEndTime()))
     ;
 
     JPAQuery<Recruitment> resultQuery = jpaQueryFactory
@@ -72,7 +73,7 @@ public class RecruitmentRepositoryCustomImpl implements RecruitmentRepositoryCus
       resultQuery.join(recruitmentIndividual).on(recruitmentIndividual.recruitment.eq(recruitment));
       countQuery.join(recruitmentIndividual).on(recruitmentIndividual.recruitment.eq(recruitment));
       whereCondition
-        .and(wageTypeEq(condition.wageType()));
+        .and(eq(recruitmentIndividual.wageType, condition.wageType()));
     }
     if (needsFoodTruckJoin) {
       resultQuery.join(recruitmentFoodTruck).on(recruitmentFoodTruck.recruitment.eq(recruitment));
@@ -137,10 +138,10 @@ public class RecruitmentRepositoryCustomImpl implements RecruitmentRepositoryCus
     BooleanBuilder whereCondition = new BooleanBuilder();
 
     whereCondition
-      .and(festivalContentIdEq(condition.festivalContentId()))
-      .and(recruitment.festival.member.username.eq(condition.festivalUserName()))
-      .and(categoryEq(condition.category()))
-      .and(statusEq(condition.status()))
+      .and(eq(recruitment.festival.contentId, condition.festivalContentId()))
+      .and(eq(recruitment.festival.member.username, condition.festivalUserName()))
+      .and(eq(recruitment.category, condition.category()))
+      .and(eq(recruitment.status, condition.status()))
       .and(isPublishedEq(condition.isPublished()))
       .and(isDeletedEq(condition.isPublished()))
     ;
@@ -196,36 +197,12 @@ public class RecruitmentRepositoryCustomImpl implements RecruitmentRepositoryCus
 
   }
 
-  private BooleanExpression festivalContentIdEq(String festivalContentId) {
-    return festivalContentId == null ? null : recruitment.festival.contentId.eq(festivalContentId);
-  }
-
   private BooleanExpression isPublishedEq(Boolean isPublished) {
     return isPublished == null ? null : recruitment.publishedAt.isNotNull().eq(isPublished);
   }
 
   private BooleanExpression isDeletedEq(Boolean isDeleted) {
     return isDeleted == null ? null : recruitment.deletedAt.isNotNull().eq(isDeleted);
-  }
-
-  private BooleanExpression regionSidoEq(String sidoCode) {
-    return sidoCode == null ? null : recruitment.sigungu.sido.code.eq(sidoCode);
-  }
-
-  private BooleanExpression regionSigunguEq(String sigunguCode) {
-    return sigunguCode == null ? null : recruitment.sigungu.code.eq(sigunguCode);
-  }
-
-  private BooleanExpression categoryEq(RecruitmentCategory category) {
-    return category == null ? null : recruitment.category.eq(category);
-  }
-
-  private BooleanExpression statusEq(RecruitmentStatus status) {
-    return status == null ? null : recruitment.status.eq(status);
-  }
-
-  private BooleanExpression wageTypeEq(RecruitmentWageType wageType) {
-    return wageType == null ? null : recruitmentIndividual.wageType.eq(wageType);
   }
 
   private BooleanExpression boothFeeRequiredEq(Boolean value) {
@@ -238,22 +215,6 @@ public class RecruitmentRepositoryCustomImpl implements RecruitmentRepositoryCus
 
   private BooleanExpression prepaidEq(Boolean value) {
     return Boolean.TRUE.equals(value) ? recruitmentFoodTruck.prepaid.isTrue() : null;
-  }
-
-  private BooleanExpression workingStartTimeGoe(LocalTime time) {
-    return time == null ? null : recruitment.workingStartTime.goe(time);
-  }
-
-  private BooleanExpression workingEndTimeLoe(LocalTime time) {
-    return time == null ? null : recruitment.workingEndTime.loe(time);
-  }
-
-  private BooleanExpression workingStartDateGoe(LocalDate date) {
-    return date == null ? null : recruitment.workingStartDate.goe(date);
-  }
-
-  private BooleanExpression workingEndDateLoe(LocalDate date) {
-    return date == null ? null : recruitment.workingEndDate.loe(date);
   }
 
 }
