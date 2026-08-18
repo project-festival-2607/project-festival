7319a8d7c61ac24c124257624d99bd77b50dbe19
mskim-17 <mskim17.public@gmail.com>
2026-08-18 14:36:52 +0900

    chore: 미사용 변수, import 정리

--- src/main/java/com/example/chook/admin/util/KeywordUtils.java
+++ src/main/java/com/example/chook/admin/util/KeywordUtils.java
@@ -2,7 +2,6 @@ package com.example.chook.admin.util;
 
 import com.example.chook.admin.entity.enums.KeywordCriteria;
 import com.example.chook.admin.entity.enums.KeywordType;
-import com.example.chook.admin.entity.enums.jobseeker.JobSeekerKeywordType;
 import com.example.chook.common.util.QuerydslUtils;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
