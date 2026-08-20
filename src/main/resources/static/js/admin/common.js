0a8d947252aabfa341208702daa1c53c7ef7dfb3
mskim-17 <mskim17.public@gmail.com>
2026-08-21 06:16:22 +0900

    feat: 열 순서 변경, 단순 클릭 시 복사 기능 추가

--- src/main/resources/static/js/admin/common.js
+++ src/main/resources/static/js/admin/common.js
@@ -2,7 +2,7 @@ document.addEventListener('click', async (event) => {
 
   const target = event.target.closest('span');
 
-  if (target?.classList.contains('click-to-copy')) {
+  if (target?.classList.contains('show-and-click-to-copy')) {
     const tooltipElement = target.querySelector('a');
     const id = tooltipElement.getAttribute('aria-describedby');
     const text = tooltipElement.dataset.bsTitle;
@@ -10,16 +10,30 @@ document.addEventListener('click', async (event) => {
       .catch(err => {
         console.error('복사 실패:', err);
       });
-    const tooltipPopup = document.getElementById(id);
-    tooltipPopup.style.setProperty('--bs-tooltip-bg', 'var(--bs-success)');
-    tooltipPopup.querySelector('.tooltip-inner').textContent = '클립보드에 복사되었습니다.';
-    await sleep(1000);
-    tooltipPopup.classList.remove('show');
-    tooltipElement.blur();
-    await sleep(150);
-    tooltipPopup.remove();
+    await successPopupMessage(tooltipElement, id);
+  }
 
+  if (target?.classList.contains('click-to-copy')) {
+    const tooltipElement = target.querySelector('a');
+    const id = tooltipElement.getAttribute('aria-describedby');
+    const value = tooltipElement.dataset.value;
+    navigator.clipboard.writeText(value)
+      .catch(err => {
+        console.error('복사 실패:', err);
+      });
+    await successPopupMessage(tooltipElement, id);
   }
 });
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+async function successPopupMessage(tooltipElement, id) {
+  const tooltipPopup = document.getElementById(id);
+  tooltipPopup.style.setProperty('--bs-tooltip-bg', 'var(--bs-success)');
+  tooltipPopup.querySelector('.tooltip-inner').textContent = '클립보드에 복사되었습니다.';
+  await sleep(1000);
+  tooltipPopup.classList.remove('show');
+  tooltipElement.blur();
+  await sleep(150);
+  tooltipPopup.remove();
+}
